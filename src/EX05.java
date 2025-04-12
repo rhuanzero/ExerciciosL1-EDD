17885fa31c2ba07a6717b9106f102830829ea4a9
rhuanzero <rhuansoaresramos@gmail.com>
2025-04-12 07:34:52 -0300

    ex05 finished

--- src/EX05.java
+++ src/EX05.java
@@ -1,14 +1,16 @@
-/*4) Utilizando as operações de manipulação de pilhas vistas em aula, assim como o código de
-PilhaGenerica visto, use uma pilha auxiliar e uma variável do tipo T, para desenvolver um
-procedimento que remova um dado objeto do tipo T de uma posição qualquer de uma pilha.
-Para saber se dois objetos do tipo T são iguais, você deve usar o método equals (ou
-compareTo). Note que você não pode acessar diretamente a estrutura interna da pilha
-(atributos), devendo usar apenas as operações (métodos) de manipulação.*/
+/*5) Escreva um programa que leia uma sequência de caracteres e determine se os parênteses,
+colchetes e chaves estão balanceados. Se a sequência não possuir esses caracteres ele
+deve ser considerado balanceado. Exemplo:
+“{ab}[cde]” - Balanceado
+“{ab[cd]efg}” - Balanceado
+“[abcde{efg]}” - Não balanceado*/
 
 public class EX05 {
     public static <T> boolean verificaBalanceamento(String str) {
     Pilha pilha1 = new Pilha();
-    boolean res = false;
+    boolean res = true;
+
+
     for (int i = 0; i < str.length(); i++) {
         if (str.charAt(i) == '(' || str.charAt(i) == ')'
                 || str.charAt(i) == '{' || str.charAt(i) == '}'
@@ -16,20 +18,42 @@ public class EX05 {
             pilha1.push(str.charAt(i));
         }
     }
+    char topo2 = pilha1.retornaTopo();
 
     while (!pilha1.vazia()){
         char topo = pilha1.pop();
-        if (topo == '('){
-            res = true;
+        if (!pilha1.vazia()) {
+            topo2 = pilha1.retornaTopo();
         }
-        else if (topo == ')'){
-
+        if (topo == ')'){
+            if (topo2 == '('){
+                res = true;
+            }
+            else {
+                res = false;
+            }
         }
-
-
+        else if (topo == ']'){
+            if(topo2 == '['){
+                res = true;
+            }
+            else {
+                res = false;
+            }
+        }
+        else if (topo == '}'){
+            if (topo2 == '{'){
+                res = true;
+            }
+            else {
+                res = false;
+            }
+        }
     }
 
     return res;
     }
 }
+
+
 
--- src/EX06.java
+++ src/EX06.java
@@ -1,12 +1,10 @@
-/*4) Utilizando as operações de manipulação de pilhas vistas em aula, assim como o código de
-PilhaGenerica visto, use uma pilha auxiliar e uma variável do tipo T, para desenvolver um
-procedimento que remova um dado objeto do tipo T de uma posição qualquer de uma pilha.
-Para saber se dois objetos do tipo T são iguais, você deve usar o método equals (ou
-compareTo). Note que você não pode acessar diretamente a estrutura interna da pilha
-(atributos), devendo usar apenas as operações (métodos) de manipulação.*/
+/*6) Elabore um método que retorne as letras invertidas das palavras de uma frase recebida
+por parâmetro, preservando a ordem das palavras na frase. Por exemplo “a maçã está podre”,
+deve ter como saída: “a ãçam átse erdop”. As operações básicas de uma pilha, push e pop,
+devem ser usadas.*/
 
 public class EX06 {
-    public static void verificaBalanceamento(String str) {
+    public static void inverteFrase(String str) {
         Pilha pilha = new Pilha();
 
         char c;
