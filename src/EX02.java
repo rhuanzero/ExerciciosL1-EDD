0d958f87969d60e1c4951f1b0deee1359b79c4b7
RHUAN SOARES RAMOS <17818786767@unirio-ad.br>
2025-04-16 17:37:39 -0300

    ex02 fixed, resoling bugs

--- src/EX02.java
+++ src/EX02.java
@@ -1,27 +1,4 @@
-/*2) Escreva uma função para determinar se uma cadeia de caracteres (string) é da forma: a D
-b D c D ... D z onde cada cadeia de caracteres, a, b, ..., z, é da forma do exercício descrito
-acima. Portanto, uma cadeia de caracteres estará no formato correto se consistir em qualquer
-número de cadeias deste tipo ( x C y ), separadas pelo caractere ‘D’. Em cada ponto, você só
-poderá ler o próximo caractere da cadeia (é mandatório o uso de pilha).*/
+package PACKAGE_NAME;
 
 public class EX02 {
-    public static boolean verificarStr(String str) {
-        PilhaGenerica<String> pilha1 = new PilhaGenerica<>(26);
-        boolean res = false;
-        String[] strtrim = str.split("D");
-
-        for (int i = 0; i < strtrim.length; i++) {
-            pilha1.push(strtrim[i]);
-        }
-
-        while (!pilha1.vazia()){
-            res = EX01.verificarxcy(pilha1.pop());
-            if (!res){return false;}
-        }
-
-        return res;
-        }
-
-    }
-
-
+}
